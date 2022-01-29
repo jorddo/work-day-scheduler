93c0d9eb248943210342ad825b8d780ca1fa570b
jorddo <jordanbsneed@gmail.com>
2022-01-29 12:52:43 -0600

    adding date to top of page

--- Develop/js/app.js
+++ Develop/js/app.js
@@ -1,21 +1,29 @@
-// 1. Display the current date at the top of the calendar
-// 	-google: how to get the current date in javascript
-// 		-how to use dates in javascript
+const currentDay = document.getElementById('currentDay');
 
-// 2. Render time blocks
+// Create current date
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy = today.getFullYear();
+today = mm + '/' + dd + '/' + yyyy;
+
+// insert date to top of calendar
+currentDay.textContent += today;
+
+// Render time blocks
 // 	-hard code in html using rows and cols
 // 	-loop over and render the table rows and cols in javascript
 
-// 3. Color-code time blocks for past, present and future
+// Color-code time blocks for past, present and future
 // 	-javascript, css
 // 	-if then statements that relate to the date and apply certain css properties
 // 	(select html elements in javascript - and add css to it)
 
-// 4. Allow user to type in event in timeblocks
+// Allow user to type in event in timeblocks
 // 	-input fields, with text saved in local storage
 // 	-attribute that indicates time-block on input fields
 // 	-On SAVE: store the date/time as the key and the event as the value
 // 		-create an event listener for the save button that puts this key,
 // 		value pair into local storage
 
-// 5. On refresh, grab the data from local storage and display it in the apppropriate time block.
+// On refresh, grab the data from local storage and display it in the apppropriate time block.
