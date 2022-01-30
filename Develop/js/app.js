576ecc2fc6e037561a5fe0d359698941338d2de4
jorddo <jordanbsneed@gmail.com>
2022-01-30 10:55:23 -0600

    starting javascript color code

--- Develop/js/app.js
+++ Develop/js/app.js
@@ -1,23 +1,21 @@
-const currentDay = document.getElementById('currentDay');
-
 // Create current date
-let today = new Date();
-let dd = String(today.getDate()).padStart(2, '0');
-let mm = String(today.getMonth() + 1).padStart(2, '0');
-let yyyy = today.getFullYear();
-today = mm + '/' + dd + '/' + yyyy;
-
-// insert date to top of calendar
-currentDay.textContent += today;
-
-// Render time blocks
-// 	-hard code in html using rows and cols
-// 	-loop over and render the table rows and cols in javascript
+let today = moment().format('MMM Do YYYY');
+// display date
+$('#currentDay').html(today);
 
 // Color-code time blocks for past, present and future
-// 	-javascript, css
-// 	-if then statements that relate to the date and apply certain css properties
-// 	(select html elements in javascript - and add css to it)
+// 	if time is equal to current hour, color red
+// else if time is more than current hour, color gray
+//else color green
+function hourVal() {
+  let currentHour = moment().hour();
+  console.log(currentHour);
+  // determination of color
+  // if () {
+  //   // need to add classes for red, gray, green
+  // }
+}
+hourVal();
 
 // Allow user to type in event in timeblocks
 // 	-input fields, with text saved in local storage
