ad7d04a7caf3bc0de4f83838eba890f7cf946761
jorddo <jordanbsneed@gmail.com>
2022-01-30 17:22:16 -0600

    time block evaluation to add/remove classes

--- Develop/js/app.js
+++ Develop/js/app.js
@@ -10,26 +10,27 @@ $('#currentDay').text(today);
 //COLOR CHANGE FUNCTION
 function hourVal() {
   let currentHour = dayjs().hour();
-  //need variable to represent time listed on block - PLACEHOLDER will represent this for now
-  $('.time-block').each(function (index) {
-    console.log(index + ': ' + $(this).text());
-  });
 
-  if (currentHour === PLACEHOLDER) {
-    $(this).addClass('present');
-    $(this).removeClass('past');
-    $(this).removeClass('future');
-  } else if (currentHour > PLACEHOLDER) {
-    $(this).addClass('past');
-    $(this).removeClass('present');
-    $(this).removeClass('future');
-  } else {
-    $(this).addClass('future');
-    $(this).removeClass('present');
-    $(this).removeClass('past');
-  }
+  // looping over blocks
+  $('.time-block').each(function () {
+    let evalHour = parseInt($(this).attr('id').split('hr')[1]);
+    if (currentHour === evalHour) {
+      $(this).addClass('present');
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+    } else if (currentHour > evalHour) {
+      $(this).addClass('past');
+      $(this).removeClass('present');
+      $(this).removeClass('future');
+    } else {
+      $(this).addClass('future');
+      $(this).removeClass('present');
+      $(this).removeClass('past');
+    }
+  });
 }
 
+hourVal();
 // Allow user to type in event in timeblocks
 // 	-input fields, with text saved in local storage
 // 	-attribute that indicates time-block on input fields
