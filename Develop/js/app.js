484e654517100412668131d9d9c0d671de21c782
jorddo <jordanbsneed@gmail.com>
2022-02-04 16:17:57 -0600

    adding loop of timeblocks

--- Develop/js/app.js
+++ Develop/js/app.js
@@ -1,41 +1,59 @@
 $(document).ready(function () {
   // Create current date
-  let today = dayjs().format('MMM-DD-YYYY');
+  const today = dayjs().format('MMM-DD-YYYY');
   // display date
   $('#currentDay').text(today);
 
+  // function to loop through time blocks and create objects with them????
+  // setting up empty value so I can manipulate it and add descriptions in
+  function storeDescriptions() {
+    const textAreas = $('.description');
+    const arr = [];
+
+    for (let i = 0; i < textAreas.length; i++) {
+      arr.push($(textAreas[i]).val());
+    }
+
+    localStorage.setItem('key', JSON.stringify(arr));
+  }
+
   //button click listener to save
   $('.saveBtn').click(function () {
-    let text = $('.txt').val();
+    // const text = $('.description').val();
+    const text = $(this).siblings('textarea').val();
     // set text inputs into storage
-    localStorage.setItem('text', JSON.stringify(text));
+    // localStorage.setItem('text', JSON.stringify(text));
+    storeDescriptions();
 
     // localStorage.setIem(key, value)
   });
 
   //COLOR CHANGE FUNCTION
   function hourVal() {
-    let currentHour = dayjs().hour();
+    const currentHour = dayjs().hour();
 
     // looping over blocks
     $('.time-block').each(function () {
-      let evalHour = parseInt($(this).attr('id').split('hr')[1]);
+      const evalHour = parseInt($(this).attr('id').split('hr')[1]);
       if (currentHour === evalHour) {
         $(this).addClass('present');
-        $(this).removeClass('past');
-        $(this).removeClass('future');
       } else if (currentHour > evalHour) {
         $(this).addClass('past');
-        $(this).removeClass('present');
-        $(this).removeClass('future');
       } else {
         $(this).addClass('future');
-        $(this).removeClass('present');
-        $(this).removeClass('past');
       }
     });
   }
 
+  function writeSchedule() {
+    const schedule = JSON.parse(localStorage.getItem('key'));
+    const textAreas = $('.description');
+
+    for (let i = 0; i < textAreas.length; i++) {
+      schedule.text($(textAreas[i]).val());
+    }
+  }
+  writeSchedule();
   hourVal();
 });
 
