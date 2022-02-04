cc29e217e02486c628d896705083dd674092ea47
jorddo <jordanbsneed@gmail.com>
2022-02-04 17:38:08 -0600

    adding loop to repopulate text areas from local storage

--- Develop/js/app.js
+++ Develop/js/app.js
@@ -1,10 +1,4 @@
 $(document).ready(function () {
-  // Create current date
-  const today = dayjs().format('MMM-DD-YYYY');
-  // display date
-  $('#currentDay').text(today);
-
-  // function to loop through time blocks and create objects with them????
   // setting up empty value so I can manipulate it and add descriptions in
   function storeDescriptions() {
     const textAreas = $('.description');
@@ -16,17 +10,6 @@ $(document).ready(function () {
 
     localStorage.setItem('key', JSON.stringify(arr));
   }
-
-  //button click listener to save
-  $('.saveBtn').click(function () {
-    // const text = $('.description').val();
-    const text = $(this).siblings('textarea').val();
-    // set text inputs into storage
-    // localStorage.setItem('text', JSON.stringify(text));
-    storeDescriptions();
-
-    // localStorage.setIem(key, value)
-  });
 
   //COLOR CHANGE FUNCTION
   function hourVal() {
@@ -49,19 +32,24 @@ $(document).ready(function () {
     const schedule = JSON.parse(localStorage.getItem('key'));
     const textAreas = $('.description');
 
-    for (let i = 0; i < textAreas.length; i++) {
-      schedule.text($(textAreas[i]).val());
+    if (schedule) {
+      for (let i = 0; i < textAreas.length; i++) {
+        $(textAreas[i]).val(schedule[i]);
+      }
     }
   }
+
+  //button click listener to save
+  $('.saveBtn').click(function () {
+    // set text inputs into storage
+    storeDescriptions();
+  });
+
+  // Create current date
+  const today = dayjs().format('MMM-DD-YYYY');
+  // display date
+  $('#currentDay').text(today);
+
   writeSchedule();
   hourVal();
 });
-
-// Allow user to type in event in timeblocks
-// 	-input fields, with text saved in local storage
-// 	-attribute that indicates time-block on input fields
-// 	-On SAVE: store the date/time as the key and the event as the value
-// 		-create an event listener for the save button that puts this key,
-// 		value pair into local storage
-
-// On refresh, grab the data from local storage and display it in the apppropriate time block.
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Work Day Scheduler Starter Code
+
+# Work Day Scheduler
+
+A simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+
+## Authors
+
+- [@jorddo](https://github.com/jorddo)
+
+
+## Acknowledgements
+
+ - [Stack Overflow](https://stackoverflow.com/)
+
+
+## Demo
+
+https://jorddo.github.io/work-day-scheduler/
+
+
+## Screenshot
+
+![work-day-scheduler](https://user-images.githubusercontent.com/94631019/152617081-f07d8249-df4d-43b1-98e8-9792f4324f20.png)
